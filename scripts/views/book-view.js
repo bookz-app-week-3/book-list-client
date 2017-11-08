de7102a842192980b5a3d7c138ba9542a91b0e4b
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2017-11-08 10:38:57 -0800

    added book view scripts

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -1,6 +1,15 @@
 app = app || {};
 
 (function (module){
+  const bookView = {};
 
-
+  bookView.initIndexPage = function() {
+    $('.container').hide;
+    $('.book-view').show;
+    app.Books.all.map(books => $('book-view').append(books.toHtml()));
+  }
+  $(document).ready(function() {
+    app.Books.fetchAll(bookView.initIndexPage);
+  });
+  module.bookView = bookView;
 })(app)
