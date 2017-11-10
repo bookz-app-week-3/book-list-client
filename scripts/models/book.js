045a03e5e034c44593b1e42ed8f7229836fcd1b3
Joel Clair <clair.joel@yahoo.com>
2017-11-10 09:02:59 -0800

    delete works

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -49,6 +49,6 @@ var __API_URL__ = 'https://jc-kn-booklist.herokuapp.com';
     $.post(`${__API_URL__}/api/v1/books`, book)
       .then(() => page('/'))
       .catch(errorCallback);
-    }
+  }
   module.Book = Book
 })(app)
